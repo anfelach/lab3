e1467f28f624ff7dcf87a53d48257363341d7dd7
anfelach <anfal.achouri@ensia.edu.dz>
2023-03-07 15:53:37 +0100

    Hello world

---
+++ src/lab_3/Start.java
@@ -0,0 +1,16 @@
+package lab_3;
+
+public class Start {
+	 public static void main(String[] args) {
+	        HelloWorld variable = new HelloWorld();
+	        variable.writeHello();
+	 }
+}
+
+
+class HelloWorld {
+    public void writeHello() {
+        System.out.print(" Hello World ");
+    }
+
+}
