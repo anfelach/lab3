91219c4ab356bb937b8098ff162c8ecf5b102ef8
anfelach <anfal.achouri@ensia.edu.dz>
2023-03-07 16:15:54 +0100

    start_class_been_modified

--- src/lab_3/Start.java
+++ src/lab_3/Start.java
@@ -1,6 +1,7 @@
 package lab_3;
 
 public class Start {
+	//the class has been modified
 	 public static void main(String[] args) {
 	        HelloWorld variable = new HelloWorld();
 	        variable.writeHello();
